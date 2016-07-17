7ddc9f505449b6533368c53306ebfd8580ca61f2
arthurpaulhs <arthur.sinaga@gmail.com>
2016-07-17 20:20:30 +0700

    Remove dependencies to WiFi manager library

--- Code/lucy_measurement/lucy_measurement.ino
+++ Code/lucy_measurement/lucy_measurement.ino
@@ -1,10 +1,18 @@
-#include <FS.h> 
 #include <ESP8266WiFi.h>          
-#include <DNSServer.h>
 #include <ESP8266WebServer.h>
-#include <WiFiManager.h>
-#include <ArduinoJson.h>
 #include <PubSubClient.h>         
+
+enum {
+  ACCESS_POINT_WEBSERVER = 0
+};
+
+ESP8266WebServer server(80);
+// soft ap ssid and pass
+const char* ssid = "esparthur";  
+const char* passphrase = "seek9cat";
+
+// broker information
+const char* mqtt_server = "iot.eclipse.org";
 
 //initialize pin
 int ledPin1 = 14;
@@ -19,22 +27,196 @@ float voltage = 0;
 int voltage_c = 0;
 int voltage_v = 0;
 char msg[50];
+String st;
+String content;
 
 //initialize client
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-//mqtt server and port decaration
-char mqtt_server[40];
-char mqtt_port[6];
+bool testWifi(void) {
+  int c = 0;
+  Serial1.println();
+  Serial1.println("System now checking for available wifi credentials and attempt to connect to it for 1 minute. Please be patient.");
+  Serial1.println();
+  Serial1.println("WIFI STATUS VALUE");
+  Serial1.println("0 : WL_IDLE_STATUS");
+  Serial1.println("1 : WL_NO_SSID_AVAIL");
+  Serial1.println("2 : WL_SCAN_COMPLETED");
+  Serial1.println("3 : WL_CONNECTED");
+  Serial1.println("4 : WL_CONNECT_FAILED");
+  Serial1.println("5 : WL_CONNECTION_LOST");
+  Serial1.println("6 : WL_DISCONNECTED");
+  Serial1.println();
+  Serial1.println("Waiting for Wifi to connect...");
+  Serial1.print("WiFi status: ");
+  while ( c < 60 ) {
+    if (WiFi.status() == WL_CONNECTED) {
+      return true;
+    }
+    delay(1000);
+    Serial1.print(WiFi.status());
+    Serial1.print(" ");
+    c++;
+  }
+  Serial1.println();
+  Serial1.println("Connection timed out, opening AP");
+  return false;
+}
 
-//flag for saving data
-bool shouldSaveConfig = false;
+void setupAccessPoint(void) {
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  int n = WiFi.scanNetworks();
+  Serial1.println("Scan done");
+  if (n == 0)
+    Serial1.println("No networks found");
+  else
+  {
+    Serial1.print(n);
+    Serial1.println(" Networks found");
+    for (int i = 0; i < n; ++i)
+    {
+      // Print SSID and RSSI for each network found
+      Serial1.print(i + 1);
+      Serial1.print(": ");
+      Serial1.print(WiFi.SSID(i));
+      Serial1.print(" (");
+      Serial1.print(WiFi.RSSI(i));
+      Serial1.print(")");
+      Serial1.println((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*");
+      delay(10);
+    }
+  }
+  Serial1.println("");
+  st = "<ol>";
+  for (int i = 0; i < n; ++i)
+  {
+    // Print SSID and RSSI for each network found
+    st += "<li>";
+    st += WiFi.SSID(i);
+    st += " (";
+    st += WiFi.RSSI(i);
+    st += ")";
+    st += (WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*";
+    st += "</li>";
+  }
+  st += "</ol>";
+  delay(100);
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, passphrase, 6);
+  launchWeb(ACCESS_POINT_WEBSERVER);
+}
 
-//callback notifying us of the need to save configuration
-void saveConfigCallback () {
-  Serial1.println("New configuration detected");
-  shouldSaveConfig = true;
+void launchWeb(int webservertype) {
+  Serial1.println();
+  setupWebServerHandlers(webservertype);
+  // Start the server
+  server.begin();
+  Serial1.print("Server type ");
+  Serial1.print(webservertype);
+  Serial1.println(" (credentials configuration) has started.");
+  Serial1.print("Local IP: ");
+  Serial1.println(WiFi.localIP());
+  Serial1.print("SoftAP IP: ");
+  Serial1.println(WiFi.softAPIP());
+}
+
+void setupWebServerHandlers(int webservertype)
+{
+  if ( webservertype == ACCESS_POINT_WEBSERVER ) {
+    server.on("/", handleDisplayAccessPoints);
+    server.on("/setap", handleSetAccessPoint);
+    server.onNotFound(handleNotFound);
+  }
+}
+
+void handleDisplayAccessPoints() {
+  IPAddress ip = WiFi.softAPIP();
+  String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+  uint8_t mac[6];
+  WiFi.macAddress(mac);
+  String macStr = macToStr(mac);
+  content = "<!DOCTYPE HTML>\n<html>Hello from ";
+  content += ssid;
+  content += " at ";
+  content += ipStr;
+  content += " (";
+  content += macStr;
+  content += ")";
+  content += "<p>";
+  content += st;
+  content += "<p><form method='get' action='setap'><label>SSID: </label>";
+  content += "<input name='ssid' length=32><input name='pass' length=64><input type='submit'></form>";
+  content += "<p>We will attempt to connect to the selected AP.";
+  content += "</html>";
+  server.send(200, "text/html", content);
+}
+
+void handleSetAccessPoint() {
+  int httpstatus = 200;
+  String qsid = server.arg("ssid");
+  String qpass = server.arg("pass");
+  if (qsid.length() > 0 && qpass.length() > 0) {
+    for (int i = 0; i < qsid.length(); i++)
+    {
+      // Deal with (potentially) plus-encoded ssid
+      qsid[i] = (qsid[i] == '+' ? ' ' : qsid[i]);
+    }
+    for (int i = 0; i < qpass.length(); i++)
+    {
+      // Deal with (potentially) plus-encoded password
+      qpass[i] = (qpass[i] == '+' ? ' ' : qpass[i]);
+    }
+    WiFi.mode(WIFI_AP_STA);
+    WiFi.begin(qsid.c_str(), qpass.c_str());
+    content = "<!DOCTYPE HTML>\n<html>";
+    content += "Connection to AP ";
+    content += qsid;
+    content += ", succedded.</html>";
+    delay(5000);
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial1.println();
+      Serial1.println("WiFi connected");
+      Serial1.print("Local IP: ");
+      Serial1.println(WiFi.localIP());
+      Serial1.print("SoftAP IP: ");
+      Serial1.println(WiFi.softAPIP());
+    }
+  } else {
+    content = "<!DOCTYPE HTML><html>";
+    content += "Error, no ssid or password set?</html>";
+    Serial1.println("Sending 404");
+    httpstatus = 404;
+  }
+  server.send(httpstatus, "text/html", content);
+}
+
+void handleNotFound() {
+  content = "File Not Found\n\n";
+  content += "URI: ";
+  content += server.uri();
+  content += "\nMethod: ";
+  content += (server.method() == HTTP_GET) ? "GET" : "POST";
+  content += "\nArguments: ";
+  content += server.args();
+  content += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    content += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", content);
+}
+
+String macToStr(const uint8_t* mac)
+{
+  String result;
+  for (int i = 0; i < 6; ++i) {
+    result += String(mac[i], 16);
+    if (i < 5)
+      result += ':';
+  }
+  return result;
 }
 
 //mqtt callback, notify when there is new messagge at subsciribed topic
@@ -118,8 +300,8 @@ void battery_voltage(){ //battery voltage measurement
   snprintf (msg, 75, "Battery voltage = %5.2f V", voltage);
   client.publish("luminocity/measurement_test/battery_voltage_measurement", msg);
   //indicator show which measurement currently executed by microprocessor
-  //digitalWrite(ledPin1, LOW);
-  //digitalWrite(ledPin2, HIGH);
+  digitalWrite(ledPin1, LOW);
+  digitalWrite(ledPin2, HIGH);
   }
 
 void solar_panel_voltage(){ //solar panel voltage measurement
@@ -139,7 +321,7 @@ void solar_panel_voltage(){ //solar panel voltage measurement
   digitalWrite(ledPin1, LOW);
   digitalWrite(ledPin2, LOW);
   }
-  
+
 void setup() {
   //pin mode
   pinMode(ledPin1, OUTPUT);
@@ -149,110 +331,25 @@ void setup() {
   pinMode(sensor_esp, INPUT);
   
   Serial1.begin(115200);
-  Serial1.println();
-
-  //read configuration from file system json
-  Serial1.println("Mounting file system...");
-
-  if (SPIFFS.begin()) {
-    Serial1.println("File system mounted ");
-    if (SPIFFS.exists("/config.json")) {
-      //file exists, reading and loading
-      Serial1.println();
-      Serial1.println("Reading configuration file");
-      File configFile = SPIFFS.open("/config.json", "r");
-      if (configFile) {
-        Serial1.println("Opened configuration file");
-        size_t size = configFile.size();
-        // Allocate a buffer to store contents of the file.
-        std::unique_ptr<char[]> buf(new char[size]);
-
-        configFile.readBytes(buf.get(), size);
-        DynamicJsonBuffer jsonBuffer;
-        JsonObject& json = jsonBuffer.parseObject(buf.get());
-        json.printTo(Serial);
-        if (json.success()) {
-          Serial1.println("\nParsed json");
-          Serial1.println();
-          strcpy(mqtt_server, json["mqtt_server"]);
-          strcpy(mqtt_port, json["mqtt_port"]);
-        } 
-        else {
-          Serial1.println("Failed to load json config");
-        }
-      }
-    }
-  } else {
-    Serial1.println("Failed to mount file system");
+  WiFi.mode(WIFI_STA);
+  if (!testWifi()) {
+    setupAccessPoint(); // No WiFi yet, enter configuration mode
   }
-  //end read
-
-  //define costum parameter
-  WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
-  WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 5);
-
-  //local intialization
-  WiFiManager wifiManager;
-
-  //set config save notify callback
-  wifiManager.setSaveConfigCallback(saveConfigCallback);
-
-  //set custom ip for portal
-  wifiManager.setAPStaticIPConfig(IPAddress(19,98,14,2), IPAddress(19,98,14,0), IPAddress(255,0,0,0));
-  
-  //add new parameters
-  wifiManager.addParameter(&custom_mqtt_server);
-  wifiManager.addParameter(&custom_mqtt_port);
-  
-  //reset settings - for testing
-  //wifiManager.resetSettings();
-
-  //set minimum quality of signal so it ignores AP's under that quality
-  wifiManager.setMinimumSignalQuality(15);
-  
-  //set timeout until configuration portal gets turned off
- wifiManager.setTimeout(600);
-
-  //fetches ssid and pass and tries to connect
-  if (!wifiManager.autoConnect("ESP_Arthur", "seek9cat")) {
-    Serial1.println("Failed to connect and hit timeout");
-    delay(3000);
-    //reset and try again, or maybe put it to deep sleep
-    ESP.reset();
-    delay(5000);
+  else{
+    Serial1.println();
+    Serial1.println("WiFi connected");
+    Serial1.print("Local IP: ");
+    Serial1.println(WiFi.localIP());
+    Serial1.print("SoftAP IP: ");
+    Serial1.println(WiFi.softAPIP());
   }
-
-  //read updated parameters
-  strcpy(mqtt_server, custom_mqtt_server.getValue());
-  strcpy(mqtt_port, custom_mqtt_port.getValue());
-  
-  //save the custom parameters to file systems
-  if (shouldSaveConfig) {
-    DynamicJsonBuffer jsonBuffer;
-    JsonObject& json = jsonBuffer.createObject();
-    json["mqtt_server"] = mqtt_server;
-    json["mqtt_port"] = mqtt_port;
-
-    File configFile = SPIFFS.open("/config.json", "w");
-    if (!configFile) {
-      Serial1.println("Failed to open configuration file for writing");
-    }
-    Serial1.println("Configuration saved");
-    json.printTo(Serial1);
-    json.printTo(configFile);
-    configFile.close();
-    //end save
-  }
-  //connected to the WiFi
-  Serial1.println();
-  Serial1.println("WiFi connected");
-
-  //mqtt setup
-  client.setServer(mqtt_server, atoi(mqtt_port));
+  client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
-} 
+}
 
 void loop() {
+  server.handleClient(); 
+  if (WiFi.status() == WL_CONNECTED){
     //connectivity test
     if (!client.connected()) {
       reconnect();
@@ -260,12 +357,13 @@ void loop() {
     client.loop();
 
     //measurement process
-    //battery_current();
-    //delay(2000);
-    //lamp_current();
-    //delay(2000);
+    battery_current();
+    delay(1000);
+    lamp_current();
+    delay(1000);
     battery_voltage();
-    delay(2000);
+    delay(1000);
     solar_panel_voltage();
-    delay(2000);
+    delay(1000);
+  }
 }
